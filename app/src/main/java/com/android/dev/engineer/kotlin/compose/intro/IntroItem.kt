01b936f90bf5a90d6170823940a0c8fee4f5cb87
android dev engineer <>
2023-03-26 22:40:39 +0100

    add intro item to populate intro screen

---
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/intro/IntroItem.kt
@@ -0,0 +1,8 @@
+package com.android.dev.engineer.kotlin.compose.intro
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class IntroItem(
+    val title: String,
+    val imageVector: ImageVector
+)
