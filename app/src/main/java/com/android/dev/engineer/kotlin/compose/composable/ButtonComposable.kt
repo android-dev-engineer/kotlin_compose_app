17d8301bfd333fdd82ddddc31dc21aef92241289
android dev engineer <>
2023-03-26 22:39:54 +0100

    add custom button composable

---
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/composable/ButtonComposable.kt
@@ -0,0 +1,20 @@
+package com.android.dev.engineer.kotlin.compose.composable
+
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ButtonComposable(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick
+    ) {
+        Text(text = text)
+    }
+}
