d0df120d20761d0a72a0d59e63312124fea0c5a0
android dev engineer <>
2023-03-29 20:35:11 +0100

    add missing compose theme

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/intro/IntroScreen.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/intro/IntroScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.dev.engineer.kotlin.compose.composable.ButtonComposable
 import com.android.dev.engineer.kotlin.compose.composable.PagerIndicatorComposable
+import com.android.dev.engineer.kotlin.compose.ui.theme.KotlinComposeAppTheme
 import kotlinx.coroutines.launch
 
 @Composable
@@ -47,58 +48,60 @@ private fun IntroScreenComposable(
     introItems: List<IntroItem>,
     onClickSkip: () -> Unit
 ) {
-    val pagerState = rememberPagerState()
-    val coroutineScope = rememberCoroutineScope()
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        HorizontalPager(
-            pageCount = introItems.size,
-            state = pagerState,
-            pageContent = { pageIndex ->
-                with(introItems[pageIndex]) {
-                    Column(
-                        modifier = Modifier
-                            .padding(all = 16.dp)
-                            .fillMaxHeight(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Image(
-                            modifier = Modifier.padding(bottom = 8.dp),
-                            contentDescription = title,
-                            imageVector = imageVector,
-                            colorFilter = ColorFilter.tint(color = MaterialTheme.colors.primary)
-                        )
-                        Text(
-                            text = title,
-                            style = MaterialTheme.typography.h5,
-                            textAlign = TextAlign.Center,
-                            color = MaterialTheme.colors.primary
-                        )
+    KotlinComposeAppTheme {
+        val pagerState = rememberPagerState()
+        val coroutineScope = rememberCoroutineScope()
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            HorizontalPager(
+                pageCount = introItems.size,
+                state = pagerState,
+                pageContent = { pageIndex ->
+                    with(introItems[pageIndex]) {
+                        Column(
+                            modifier = Modifier
+                                .padding(all = 16.dp)
+                                .fillMaxHeight(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Image(
+                                modifier = Modifier.padding(bottom = 8.dp),
+                                contentDescription = title,
+                                imageVector = imageVector,
+                                colorFilter = ColorFilter.tint(color = MaterialTheme.colors.primary)
+                            )
+                            Text(
+                                text = title,
+                                style = MaterialTheme.typography.h5,
+                                textAlign = TextAlign.Center,
+                                color = MaterialTheme.colors.primary
+                            )
+                        }
                     }
                 }
-            }
-        )
+            )
 
-        ButtonComposable(
-            modifier = Modifier
-                .padding(all = 16.dp)
-                .align(alignment = Alignment.TopEnd),
-            text = "Skip",
-            onClick = { onClickSkip() }
-        )
+            ButtonComposable(
+                modifier = Modifier
+                    .padding(all = 16.dp)
+                    .align(alignment = Alignment.TopEnd),
+                text = "Skip",
+                onClick = { onClickSkip() }
+            )
 
-        PagerIndicatorComposable(
-            modifier = Modifier
-                .padding(bottom = 16.dp)
-                .align(alignment = Alignment.BottomCenter),
-            selectedIndex = pagerState.currentPage,
-            total = introItems.size,
-            onClick = { index ->
-                coroutineScope.launch { pagerState.scrollToPage(index) }
-            }
-        )
+            PagerIndicatorComposable(
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .align(alignment = Alignment.BottomCenter),
+                selectedIndex = pagerState.currentPage,
+                total = introItems.size,
+                onClick = { index ->
+                    coroutineScope.launch { pagerState.scrollToPage(index) }
+                }
+            )
+        }
     }
 }
 
