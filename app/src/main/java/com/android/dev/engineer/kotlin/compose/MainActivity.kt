b0863d1b3d363e41ccd06e652b2cf8bca6fefa3b
android dev engineer <>
2023-03-31 21:16:55 +0100

    update main activity to handle navigation

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/MainActivity.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/MainActivity.kt
@@ -3,20 +3,40 @@ package com.android.dev.engineer.kotlin.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.android.dev.engineer.kotlin.compose.data.domain.MainNavGraph
+import com.android.dev.engineer.kotlin.compose.sign_in.SignInScreen
 import com.android.dev.engineer.kotlin.compose.intro.IntroScreen
-import com.android.dev.engineer.kotlin.compose.ui.theme.KotlinComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            KotlinComposeAppTheme {
-                IntroScreen(
-                    onClickSkip = {
-                        // TODO Implement skip functionality, this is gonna be included with navigation compose
-                    }
-                )
-            }
+            // TODO get start destination via bundle
+            MainNavHost(startDestination = MainNavGraph.Intro.route)
         }
+    }
+}
+
+@Composable
+fun MainNavHost(startDestination: String) {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(route = MainNavGraph.Intro.route) {
+            IntroScreen(
+                onSkipClicked = {
+                    navController.navigate(it.route) {
+                        popUpTo(route = MainNavGraph.Intro.route) { inclusive = true }
+                    }
+                }
+            )
+        }
+        composable(MainNavGraph.SignIn.route) { SignInScreen() }
     }
 }
