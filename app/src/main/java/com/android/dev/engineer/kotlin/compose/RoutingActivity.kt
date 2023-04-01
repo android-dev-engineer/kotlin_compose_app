c3372855881df07eccb208b59ca6df0f1c18e6b2
android dev engineer <>
2023-04-01 20:30:24 +0100

    update routing to include viewmodel to determine when the intro screen should be displayed

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/RoutingActivity.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/RoutingActivity.kt
@@ -3,31 +3,34 @@ package com.android.dev.engineer.kotlin.compose
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.android.dev.engineer.kotlin.compose.data.domain.MainNavGraph
-import kotlinx.coroutines.delay
+import com.android.dev.engineer.kotlin.compose.feature.routing.RoutingViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
-import java.util.concurrent.TimeUnit
 
+@AndroidEntryPoint
 class RoutingActivity : ComponentActivity() {
+    private val viewModel by viewModels<RoutingViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         splashScreen.setKeepOnScreenCondition { true }
-        initDelay()
+        setUpObservers()
     }
 
-    // TODO remove it after adding ViewModel
-    private fun initDelay() {
+    private fun setUpObservers() {
         lifecycleScope.launch {
-            delay(TimeUnit.SECONDS.toMillis(1))
-            navigateToMain()
+            viewModel.effect.flowWithLifecycle(lifecycle).collect(::updateEffect)
         }
     }
 
-    private fun navigateToMain() {
-        val bundle = MainActivity.prepareBundle(MainNavGraph.Intro.route)
+    private fun updateEffect(mainNavGraph: MainNavGraph) {
+        val bundle = MainActivity.prepareBundle(startDestination = mainNavGraph.route)
         val intent = Intent(this, MainActivity::class.java).apply {
             putExtras(bundle)
         }
---
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/routing/RoutingViewModel.kt
@@ -0,0 +1,34 @@
+package com.android.dev.engineer.kotlin.compose.feature.routing
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.dev.engineer.kotlin.compose.data.domain.MainNavGraph
+import com.android.dev.engineer.kotlin.compose.data.use_case.initial_route.GetInitialRouteUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RoutingViewModel @Inject constructor(
+    private val getInitialRouteUseCase: GetInitialRouteUseCase,
+) : ViewModel() {
+    private val _effect by lazy { MutableSharedFlow<MainNavGraph>() }
+    val effect = _effect.asSharedFlow()
+
+    init {
+        getInitialRoute()
+    }
+
+    private fun getInitialRoute() {
+        viewModelScope.launch {
+            try {
+                val mainNavGraph = getInitialRouteUseCase.invoke()
+                _effect.emit(mainNavGraph)
+            } catch (ex: Exception) {
+                // TODO add log
+            }
+        }
+    }
+}
