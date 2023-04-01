69098d88b1e9c0955c3d5ca3a555ad8e3a61852c
android dev engineer <>
2023-04-01 21:02:31 +0100

    add routing screen delay

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/routing/RoutingViewModel.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/routing/RoutingViewModel.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalTime::class)
+
 package com.android.dev.engineer.kotlin.compose.feature.routing
 
 import androidx.lifecycle.ViewModel
@@ -5,15 +7,22 @@ import androidx.lifecycle.viewModelScope
 import com.android.dev.engineer.kotlin.compose.data.domain.MainNavGraph
 import com.android.dev.engineer.kotlin.compose.data.use_case.initial_route.GetInitialRouteUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
 @HiltViewModel
 class RoutingViewModel @Inject constructor(
     private val getInitialRouteUseCase: GetInitialRouteUseCase
 ) : ViewModel() {
+    companion object {
+        private const val ROUTING_DELAY = 1000
+    }
+
     private val _effect by lazy { MutableSharedFlow<MainNavGraph>() }
     val effect = _effect.asSharedFlow()
 
@@ -24,7 +33,10 @@ class RoutingViewModel @Inject constructor(
     private fun getInitialRoute() {
         viewModelScope.launch {
             try {
-                val mainNavGraph = getInitialRouteUseCase.invoke()
+                val (mainNavGraph, duration) = measureTimedValue {
+                    getInitialRouteUseCase.invoke()
+                }
+                delay(ROUTING_DELAY - duration.inWholeMilliseconds)
                 _effect.emit(mainNavGraph)
             } catch (ex: Exception) {
                 // TODO add log
