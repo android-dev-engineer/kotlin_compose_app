2b39a37af9243438fb0bf8a56e8ba973e9c0cca0
android dev engineer <>
2023-04-01 20:26:48 +0100

    add mark intro as complete use case

---
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/data/use_case/mark_intro/MarkIntroCompleteUseCaseImpl.kt
@@ -0,0 +1,16 @@
+package com.android.dev.engineer.kotlin.compose.data.use_case.mark_intro
+
+import com.android.dev.engineer.kotlin.compose.data.di.IoDispatcher
+import com.android.dev.engineer.kotlin.compose.data.local.AppDataStore
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class MarkIntroCompleteUseCaseImpl @Inject constructor(
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    private val appDataStore: AppDataStore
+) : MarkIntroCompleteUseCase {
+    override suspend fun invoke() = withContext(ioDispatcher) {
+        appDataStore.markIntroComplete()
+    }
+}
