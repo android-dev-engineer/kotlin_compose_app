08625225129160fdadcdd69cf0881656b8b7d479
android dev engineer <>
2023-04-01 20:32:43 +0100

    move routing activity to the correct package

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/routing/RoutingActivity.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/routing/RoutingActivity.kt
@@ -1,4 +1,4 @@
-package com.android.dev.engineer.kotlin.compose
+package com.android.dev.engineer.kotlin.compose.feature.routing
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,8 +7,8 @@ import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.android.dev.engineer.kotlin.compose.MainActivity
 import com.android.dev.engineer.kotlin.compose.data.domain.MainNavGraph
-import com.android.dev.engineer.kotlin.compose.feature.routing.RoutingViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
