660051f4db3401acd217e9705479c5366873b1e7
android dev engineer <>
2023-04-01 20:35:15 +0100

    add text button composable

---
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/composable/TextButtonComposable.kt
@@ -0,0 +1,30 @@
+package com.android.dev.engineer.kotlin.compose.composable
+
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ButtonComposable(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick
+    ) {
+        Text(text = text)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewButtonComposable() {
+    ButtonComposable(
+        text = "Skip",
+        onClick = {}
+    )
+}
