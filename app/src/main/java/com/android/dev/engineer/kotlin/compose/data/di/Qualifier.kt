1772b3a86ece986be9b88b432cdcdbbb022ec0ad
android dev engineer <>
2023-04-01 20:25:39 +0100

    add di qualifiers

---
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/data/di/Qualifier.kt
@@ -0,0 +1,11 @@
+package com.android.dev.engineer.kotlin.compose.data.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class AppPrefs
