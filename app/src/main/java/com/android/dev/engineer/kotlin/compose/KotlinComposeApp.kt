f7c7ccb7516786c90dca47f5c13fae2392d46db0
android dev engineer <>
2023-04-01 20:32:02 +0100

    add kotlin compose app class to support hilt

---
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/KotlinComposeApp.kt
@@ -0,0 +1,7 @@
+package com.android.dev.engineer.kotlin.compose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class KotlinComposeApp : Application()
