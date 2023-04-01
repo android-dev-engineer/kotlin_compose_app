4322aad9c4ad4eec2569071bf334976f38462b41
android dev engineer <>
2023-04-01 20:58:03 +0100

    add intro viewmodel

---
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/intro/IntroViewModel.kt
@@ -0,0 +1,30 @@
+package com.android.dev.engineer.kotlin.compose.feature.intro
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.dev.engineer.kotlin.compose.data.domain.MainNavGraph
+import com.android.dev.engineer.kotlin.compose.data.use_case.mark_intro.MarkIntroCompleteUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class IntroViewModel @Inject constructor(
+    private val markIntroCompleteUseCase: MarkIntroCompleteUseCase
+) : ViewModel() {
+    private val _effect by lazy { MutableSharedFlow<IntroEffect>() }
+    val effect = _effect.asSharedFlow()
+
+    fun markIntroComplete() {
+        viewModelScope.launch {
+            try {
+                markIntroCompleteUseCase.invoke()
+                _effect.emit(IntroEffect.NavigateTo(MainNavGraph.SignIn))
+            } catch (_: Exception) {
+                // TODO add log
+            }
+        }
+    }
+}
