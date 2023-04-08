1ebde7661a3f6e263012531cc0e206324c92f2bd
android dev engineer <>
2023-04-08 21:05:53 +0100

    add unit tests to IntroViewModel

---
+++ app/src/test/java/com/android/dev/engineer/kotlin/compose/composable/viewmodel/IntroViewModelTest.kt
@@ -0,0 +1,35 @@
+package com.android.dev.engineer.kotlin.compose.composable.viewmodel
+
+import com.android.dev.engineer.kotlin.compose.composable.coroutines.MainTestRule
+import com.android.dev.engineer.kotlin.compose.composable.fake.MarkIntroCompleteUseFake
+import com.android.dev.engineer.kotlin.compose.data.domain.MainNavGraph
+import com.android.dev.engineer.kotlin.compose.feature.intro.IntroViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class IntroViewModelTest {
+    @get:Rule
+    val mainTestRule: MainTestRule = MainTestRule()
+    private lateinit var markIntroCompleteUseCase: MarkIntroCompleteUseFake
+    private lateinit var viewModel: IntroViewModel
+
+    @Before
+    fun setUp() {
+        markIntroCompleteUseCase = MarkIntroCompleteUseFake()
+        viewModel = IntroViewModel(markIntroCompleteUseCase = markIntroCompleteUseCase)
+    }
+
+    @Test
+    fun `test mark intro as complete`() = runTest {
+        markIntroCompleteUseCase.markIntroComplete = false
+        viewModel.markIntroComplete()
+        assertEquals(MainNavGraph.SignIn, viewModel.effect.first())
+        assertEquals(true, markIntroCompleteUseCase.markIntroComplete)
+    }
+}
