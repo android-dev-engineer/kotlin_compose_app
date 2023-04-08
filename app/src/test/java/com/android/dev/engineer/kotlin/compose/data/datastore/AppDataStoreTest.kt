226eb43a73695b6e40dff54622626fccfa38f4e8
android dev engineer <>
2023-04-08 22:11:49 +0100

    add unit tests to AppDataStore

---
+++ app/src/test/java/com/android/dev/engineer/kotlin/compose/data/datastore/AppDataStoreTest.kt
@@ -0,0 +1,49 @@
+package com.android.dev.engineer.kotlin.compose.data.datastore
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.test.core.app.ApplicationProvider
+import com.android.dev.engineer.kotlin.compose.coroutines.MainTestRule
+import com.android.dev.engineer.kotlin.compose.data.di.DataStoreModule.provideAppPrefsDataStore
+import com.android.dev.engineer.kotlin.compose.data.local.AppDataStore
+import com.android.dev.engineer.kotlin.compose.data.local.AppDataStoreImpl
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(RobolectricTestRunner::class)
+class AppDataStoreTest {
+    @get:Rule
+    val mainTestRule: MainTestRule = MainTestRule()
+
+    private lateinit var dataStore: DataStore<Preferences>
+    private lateinit var appDataStore: AppDataStore
+
+    @Before
+    fun setUp() {
+        dataStore = provideAppPrefsDataStore(
+            coroutineScope = CoroutineScope(mainTestRule.testScope.coroutineContext + Job()),
+            appContext = ApplicationProvider.getApplicationContext()
+        )
+        appDataStore = AppDataStoreImpl(dataStore = dataStore)
+    }
+
+    @Test
+    fun `test intro is pending`() = runTest {
+        assertEquals(true, appDataStore.isIntroPending())
+    }
+
+    @Test
+    fun `test when intro is complete`() = runTest {
+        appDataStore.markIntroComplete()
+        assertEquals(false, appDataStore.isIntroPending())
+    }
+}
