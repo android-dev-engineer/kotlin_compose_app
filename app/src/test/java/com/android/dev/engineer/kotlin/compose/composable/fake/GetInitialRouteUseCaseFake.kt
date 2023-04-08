3e1a8d2a1ae4f4bdd5764e051ccc1761158bf1a9
android dev engineer <>
2023-04-08 21:04:21 +0100

    add GetInitialRouteUseFake for test use

---
+++ app/src/test/java/com/android/dev/engineer/kotlin/compose/composable/fake/GetInitialRouteUseCaseFake.kt
@@ -0,0 +1,15 @@
+package com.android.dev.engineer.kotlin.compose.composable.fake
+
+import com.android.dev.engineer.kotlin.compose.data.domain.MainNavGraph
+import com.android.dev.engineer.kotlin.compose.data.use_case.initial_route.GetInitialRouteUseCase
+
+class GetInitialRouteUseCaseFake : GetInitialRouteUseCase {
+    lateinit var mainNavGraph: MainNavGraph
+    var exception: Exception? = null
+
+    override suspend fun invoke(): MainNavGraph {
+        throw checkNotNull(exception) {
+            return mainNavGraph
+        }
+    }
+}
