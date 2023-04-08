6fb77a07e866ef184e56d44ee461f1226f0d410b
android dev engineer <>
2023-04-08 21:05:37 +0100

    add unit tests to MarkIntroCompleteUseCase

---
+++ app/src/test/java/com/android/dev/engineer/kotlin/compose/composable/MarkIntroCompleteUseCaseTest.kt
@@ -0,0 +1,36 @@
+package com.android.dev.engineer.kotlin.compose.composable
+
+import com.android.dev.engineer.kotlin.compose.composable.coroutines.MainTestRule
+import com.android.dev.engineer.kotlin.compose.composable.fake.AppDataStoreFake
+import com.android.dev.engineer.kotlin.compose.data.use_case.mark_intro.MarkIntroCompleteUseCase
+import com.android.dev.engineer.kotlin.compose.data.use_case.mark_intro.MarkIntroCompleteUseCaseImpl
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class MarkIntroCompleteUseCaseTest {
+    @get:Rule
+    val mainTestRule: MainTestRule = MainTestRule()
+    private lateinit var appDataStore: AppDataStoreFake
+    private lateinit var markIntroCompleteUseCase: MarkIntroCompleteUseCase
+
+    @Before
+    fun setUp() {
+        appDataStore = AppDataStoreFake()
+        markIntroCompleteUseCase = MarkIntroCompleteUseCaseImpl(
+            dispatcher = mainTestRule.testDispatcher,
+            appDataStore = appDataStore
+        )
+    }
+
+    @Test
+    fun `test mark intro is updated`() = runTest {
+        assertEquals(false, appDataStore.markIntroComplete)
+        markIntroCompleteUseCase.invoke()
+        assertEquals(true, appDataStore.markIntroComplete)
+    }
+}
