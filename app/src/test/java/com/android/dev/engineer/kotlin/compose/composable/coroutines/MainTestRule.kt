f69fc1be038b4fa239c7bac3b12187c25a5a5cc5
android dev engineer <>
2023-04-08 21:01:35 +0100

    add MainTestRule to support coroutines for test use

---
+++ app/src/test/java/com/android/dev/engineer/kotlin/compose/composable/coroutines/MainTestRule.kt
@@ -0,0 +1,22 @@
+package com.android.dev.engineer.kotlin.compose.composable.coroutines
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@ExperimentalCoroutinesApi
+class MainTestRule(
+    val testDispatcher: TestDispatcher = StandardTestDispatcher()
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
