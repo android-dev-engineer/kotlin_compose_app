c18b9fe9903896d46ecb6402182f02b10f072942
android dev engineer <>
2023-04-09 17:22:12 +0100

    rename use case

--- app/src/test/java/com/android/dev/engineer/kotlin/compose/fake/MarkIntroCompleteUseCaseFake.kt
+++ app/src/test/java/com/android/dev/engineer/kotlin/compose/fake/MarkIntroCompleteUseCaseFake.kt
@@ -2,7 +2,7 @@ package com.android.dev.engineer.kotlin.compose.fake
 
 import com.android.dev.engineer.kotlin.compose.data.use_case.mark_intro.MarkIntroCompleteUseCase
 
-class MarkIntroCompleteUseFake : MarkIntroCompleteUseCase {
+class MarkIntroCompleteUseCaseFake : MarkIntroCompleteUseCase {
     var markIntroComplete = false
     var error: Exception? = null
 
