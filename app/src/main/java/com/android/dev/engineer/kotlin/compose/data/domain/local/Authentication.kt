fe066829c5d837bd9bb3134699094c6af8eec781
android dev engineer <>
2023-04-17 21:10:27 +0100

    add domain classes to handle network layer

---
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/data/domain/local/Authentication.kt
@@ -0,0 +1,6 @@
+package com.android.dev.engineer.kotlin.compose.data.domain.local
+
+data class Authentication(
+    val username: String,
+    val password: String
+)
---
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/data/domain/network/Authenticated.kt
@@ -0,0 +1,14 @@
+package com.android.dev.engineer.kotlin.compose.data.domain.network
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Authenticated(
+    @Json(name = "success")
+    val success: Boolean,
+    @Json(name = "expires_at")
+    val expiresAt: String,
+    @Json(name = "request_token")
+    val requestToken: String
+)
