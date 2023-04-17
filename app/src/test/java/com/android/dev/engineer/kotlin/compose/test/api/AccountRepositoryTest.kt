e9ec558222ffd7d79f95607f335761b441a323bd
android dev engineer <>
2023-04-17 22:26:43 +0100

    merge unit test to simplify

--- app/src/test/java/com/android/dev/engineer/kotlin/compose/test/api/AccountRepositoryTest.kt
+++ app/src/test/java/com/android/dev/engineer/kotlin/compose/test/api/AccountRepositoryTest.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package com.android.dev.engineer.kotlin.compose.test.api
 
 import com.android.dev.engineer.kotlin.compose.data.api.authentication.AuthenticationRepository
@@ -10,7 +12,8 @@ import com.android.dev.engineer.kotlin.compose.extension.unifiedApi
 import com.android.dev.engineer.kotlin.compose.fake.domain.SessionFake.createNewSession
 import com.android.dev.engineer.kotlin.compose.fake.domain.SessionFake.createSession
 import com.android.dev.engineer.kotlin.compose.util.FileResourceUtil
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
@@ -18,6 +21,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class AccountRepositoryTest {
     private lateinit var mockWebServer: MockWebServer
     private lateinit var authenticationRepository: AuthenticationRepository
@@ -35,18 +39,7 @@ class AccountRepositoryTest {
     }
 
     @Test
-    fun `verify get new session http method`(): Unit = runBlocking {
-        val bodyResponse = createNewSession().toJson()
-        val mockResponse = MockResponse().setBody(bodyResponse)
-        mockWebServer.enqueue(mockResponse)
-        authenticationRepository.getNewSession(session = createSession())
-
-        val request = mockWebServer.takeRequest()
-        assertEquals("POST", request.method)
-    }
-
-    @Test
-    fun `verify get new session url path`(): Unit = runBlocking {
+    fun `verify get new session url`() = runTest {
         val bodyResponse = createNewSession().toJson()
         val mockResponse = MockResponse().setBody(bodyResponse)
         mockWebServer.enqueue(mockResponse)
@@ -54,11 +47,12 @@ class AccountRepositoryTest {
 
         val request = mockWebServer.takeRequest()
         val requestUrl = request.requestUrl!!
+        assertEquals("POST", request.method)
         assertEquals("/authentication/session/new", requestUrl.encodedPath)
     }
 
     @Test
-    fun `verify get new session request`(): Unit = runBlocking {
+    fun `verify get new session request`() = runTest {
         val expectedSession = FileResourceUtil.getContent(file = "json/session.json").fromJson<Session>()
         val bodyResponse = FileResourceUtil.getContent(file = "json/new_session.json")
         val mockResponse = MockResponse().setBody(bodyResponse)
@@ -70,7 +64,7 @@ class AccountRepositoryTest {
     }
 
     @Test
-    fun `verify get new session response`(): Unit = runBlocking {
+    fun `verify get new session response`() = runTest {
         val bodyJson = FileResourceUtil.getContent(file = "json/new_session.json")
         val mockResponse = MockResponse().setBody(bodyJson)
         mockWebServer.enqueue(mockResponse)
