4c691c3ad0e0e4017eab9a7844f476cc20ac47cc
android dev engineer <>
2023-04-20 18:39:32 +0100

    rename class

--- app/src/test/java/com/android/dev/engineer/kotlin/compose/test/api/AuthenticationRepositoryTest.kt
+++ app/src/test/java/com/android/dev/engineer/kotlin/compose/test/api/AuthenticationRepositoryTest.kt
@@ -22,7 +22,7 @@ import org.junit.Before
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
-class AccountRepositoryTest {
+class AuthenticationRepositoryTest {
     private lateinit var mockWebServer: MockWebServer
     private lateinit var authenticationRepository: AuthenticationRepository
 
