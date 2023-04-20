58a79938dabf7a275735921ad540baebf6fe9433
android dev engineer <>
2023-04-20 18:58:29 +0100

    simplify code

--- app/src/test/java/com/android/dev/engineer/kotlin/compose/extension/MockWebServerExt.kt
+++ app/src/test/java/com/android/dev/engineer/kotlin/compose/extension/MockWebServerExt.kt
@@ -1,20 +1,19 @@
 package com.android.dev.engineer.kotlin.compose.extension
 
 import com.android.dev.engineer.kotlin.compose.data.api.TheMovieApi
-import com.android.dev.engineer.kotlin.compose.data.di.NetworkModule
+import com.android.dev.engineer.kotlin.compose.data.di.NetworkModule.provideMoshi
+import com.android.dev.engineer.kotlin.compose.data.di.NetworkModule.provideMoshiConverterFactory
+import com.android.dev.engineer.kotlin.compose.data.di.NetworkModule.provideOkHttpClient
 import com.android.dev.engineer.kotlin.compose.data.di.NetworkModule.provideRetrofitBuilder
+import com.android.dev.engineer.kotlin.compose.data.di.NetworkModule.provideTheMovieApi
 import com.android.dev.engineer.kotlin.compose.data.interceptor.ApiKeyInterceptor
 import okhttp3.mockwebserver.MockWebServer
 
 fun MockWebServer.toTheMovieApi(): TheMovieApi {
-    val moshi = NetworkModule.provideMoshi()
-    val moshiConverterFactory = NetworkModule.provideMoshiConverterFactory(moshi = moshi)
-    val okHttpClient = NetworkModule.provideOkHttpClient(apiKeyInterceptor = ApiKeyInterceptor())
     val retrofitBuilder = provideRetrofitBuilder(
         baseUrl = url("").toString(),
-        okHttpClient = okHttpClient,
-        moshiConverterFactory = moshiConverterFactory
-
+        okHttpClient = provideOkHttpClient(apiKeyInterceptor = ApiKeyInterceptor()),
+        moshiConverterFactory = provideMoshiConverterFactory(moshi = provideMoshi())
     )
-    return NetworkModule.provideTheMovieApi(retrofitBuilder = retrofitBuilder)
+    return provideTheMovieApi(retrofitBuilder = retrofitBuilder)
 }
