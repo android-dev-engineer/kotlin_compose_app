45d318038ca50a95342561f02b7ef208da215cfa
android dev engineer <>
2023-04-20 20:46:18 +0100

    rename di module

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/data/di/RepositoryModule.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/data/di/RepositoryModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DataModule {
+object RepositoryModule {
     @Singleton
     @Provides
     fun provideAuthenticationRepository(authenticationRepositoryImpl: AuthenticationRepositoryImpl): AuthenticationRepository {
