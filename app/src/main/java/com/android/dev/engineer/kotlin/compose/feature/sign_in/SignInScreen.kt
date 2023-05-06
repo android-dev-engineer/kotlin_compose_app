fde8930913b6781ae593c45be44d0a95069438c3
android dev engineer <>
2023-05-06 23:00:47 +0100

    add temporary action to sign in screen

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/sign_in/SignInScreen.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/sign_in/SignInScreen.kt
@@ -2,29 +2,37 @@ package com.android.dev.engineer.kotlin.compose.feature.sign_in
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.android.dev.engineer.kotlin.compose.data.domain.local.MainNavGraph
+import com.android.dev.engineer.kotlin.compose.ui.composable.ButtonComposable
 import com.android.dev.engineer.kotlin.compose.ui.theme.KotlinComposeAppTheme
 import com.android.dev.engineer.kotlin.compose.util.ExcludeFromJacocoGeneratedReport
 
 @Composable
-fun SignInScreen() {
-    SignInScreenComposable()
+fun SignInScreen(
+    onLoggedIn: (MainNavGraph) -> Unit
+) {
+    SignInScreenComposable(
+        onLoggedIn = onLoggedIn
+    )
 }
 
 // TODO implement sign in screen
 @Composable
-fun SignInScreenComposable() {
+fun SignInScreenComposable(
+    onLoggedIn: (MainNavGraph) -> Unit
+) {
     KotlinComposeAppTheme {
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = "Coming soon"
+            ButtonComposable(
+                text = "Go to upcoming movies",
+                onClick = { onLoggedIn(MainNavGraph.UpcomingMovies) }
             )
         }
     }
@@ -34,5 +42,7 @@ fun SignInScreenComposable() {
 @Composable
 @ExcludeFromJacocoGeneratedReport
 private fun PreviewSignInScreenComposable() {
-    SignInScreenComposable()
+    SignInScreenComposable(
+        onLoggedIn = {}
+    )
 }
