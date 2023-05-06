f01b7bf5a6e95384de7362632a640600f041fbc3
android dev engineer <>
2023-05-06 22:58:34 +0100

    add movie item to sanitise data

---
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/data/domain/local/MovieItem.kt
@@ -0,0 +1,28 @@
+package com.android.dev.engineer.kotlin.compose.data.domain.local
+
+import com.android.dev.engineer.kotlin.compose.data.api.TheMovieApi.Companion.IMAGE_URL
+import com.android.dev.engineer.kotlin.compose.data.domain.network.Movie
+
+data class MovieItem(
+    val id: Int,
+    val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    val posterPath: String,
+    val releaseDate: String,
+    val title: String,
+    val voteAverage: Double,
+    val voteCount: Int
+)
+
+fun Movie.toMovieItem() = MovieItem(
+    id = id,
+    originalTitle = originalTitle,
+    overview = overview,
+    popularity = popularity,
+    posterPath = posterPath?.let { IMAGE_URL + it }.orEmpty(),
+    releaseDate = releaseDate,
+    title = title,
+    voteAverage = voteAverage,
+    voteCount = voteCount
+)
