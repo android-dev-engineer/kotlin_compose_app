371a3b9ed1a12bc44a324e99aa072179485f8d2a
android dev engineer <>
2023-05-11 18:03:23 +0100

    add login and request token apis

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/data/api/authentication/AuthenticationApi.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/data/api/authentication/AuthenticationApi.kt
@@ -1,11 +1,20 @@
 package com.android.dev.engineer.kotlin.compose.data.api.authentication
 
+import com.android.dev.engineer.kotlin.compose.data.domain.network.Authenticated
 import com.android.dev.engineer.kotlin.compose.data.domain.network.NewSession
 import com.android.dev.engineer.kotlin.compose.data.domain.network.Session
+import com.android.dev.engineer.kotlin.compose.data.domain.network.SignIn
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AuthenticationApi {
+    @GET("authentication/token/new")
+    suspend fun getRequestToken(): Authenticated
+
+    @POST("authentication/token/validate_with_login")
+    suspend fun login(@Body signIn: SignIn): Authenticated
+
     @POST("authentication/session/new")
     suspend fun getNewSession(@Body session: Session): NewSession
 }
