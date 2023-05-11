ccf21196654dc82b2ed9f6712ded7f43471dc05d
android dev engineer <>
2023-05-11 18:04:42 +0100

    include login and request token api integration

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/data/api/authentication/AuthenticationRepository.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/data/api/authentication/AuthenticationRepository.kt
@@ -1,8 +1,12 @@
 package com.android.dev.engineer.kotlin.compose.data.api.authentication
 
+import com.android.dev.engineer.kotlin.compose.data.domain.network.Authenticated
 import com.android.dev.engineer.kotlin.compose.data.domain.network.NewSession
 import com.android.dev.engineer.kotlin.compose.data.domain.network.Session
+import com.android.dev.engineer.kotlin.compose.data.domain.network.SignIn
 
 interface AuthenticationRepository {
     suspend fun getNewSession(session: Session): NewSession
+    suspend fun getRequestToken(): Authenticated
+    suspend fun login(signIn: SignIn): Authenticated
 }
