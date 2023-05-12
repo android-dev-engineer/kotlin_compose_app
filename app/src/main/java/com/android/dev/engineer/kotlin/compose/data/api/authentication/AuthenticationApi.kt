afbf15b1c54cb05282818078fc7327faea2b4881
android dev engineer <>
2023-05-12 18:51:41 +0100

    add logout api integration

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/data/api/authentication/AuthenticationApi.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/data/api/authentication/AuthenticationApi.kt
@@ -1,11 +1,9 @@
 package com.android.dev.engineer.kotlin.compose.data.api.authentication
 
-import com.android.dev.engineer.kotlin.compose.data.domain.network.Authenticated
-import com.android.dev.engineer.kotlin.compose.data.domain.network.NewSession
-import com.android.dev.engineer.kotlin.compose.data.domain.network.Session
-import com.android.dev.engineer.kotlin.compose.data.domain.network.SignIn
+import com.android.dev.engineer.kotlin.compose.data.domain.network.*
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.HTTP
 import retrofit2.http.POST
 
 interface AuthenticationApi {
@@ -17,4 +15,8 @@ interface AuthenticationApi {
 
     @POST("authentication/session/new")
     suspend fun getNewSession(@Body session: Session): NewSession
+
+    // Delete with body: https://square.github.io/retrofit/2.x/retrofit/retrofit2/http/HTTP.html
+    @HTTP(method = "DELETE", path = "authentication/session", hasBody = true)
+    suspend fun logOut(@Body logOut: LogOut)
 }
