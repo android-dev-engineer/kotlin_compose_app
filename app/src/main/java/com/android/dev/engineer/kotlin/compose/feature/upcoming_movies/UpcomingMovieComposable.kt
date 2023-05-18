4fae819b8ffc168e5233c2bc8039eefbdeb2e4f6
android dev engineer <>
2023-05-18 19:13:20 +0100

    make movie title with 2 max lines

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/upcoming_movies/UpcomingMovieComposable.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/upcoming_movies/UpcomingMovieComposable.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -67,7 +68,9 @@ fun MovieItemComposable(
                     text = movieItem.originalTitle,
                     fontWeight = FontWeight.Bold,
                     color = colorResource(id = R.color.white),
-                    fontSize = MaterialTheme.typography.body1.fontSize
+                    fontSize = MaterialTheme.typography.body1.fontSize,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
                 )
                 Text(
                     modifier = Modifier.fillMaxWidth(),
