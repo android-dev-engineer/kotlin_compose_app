4dcdb35f5a48a7f9d53b4e7551f3affe816cfc88
android dev engineer <>
2023-05-18 19:44:55 +0100

    add text when no more pages are available

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/upcoming_movies/UpcomingMoviesComposable.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/feature/upcoming_movies/UpcomingMoviesComposable.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
@@ -92,30 +93,49 @@ fun MovieListComposable(
                         }
                     }
                 )
-                if (lazyPagingItems.loadState.append == LoadState.Loading) {
-                    item(
-                        span = { GridItemSpan(currentLineSpan = columnsSize) },
-                        content = {
-                            CircularProgressIndicator(
-                                modifier = Modifier
-                                    .padding(all = 8.dp)
-                                    .wrapContentWidth(align = Alignment.CenterHorizontally)
-                            )
-                        }
-                    )
-                } else if (lazyPagingItems.loadState.append is LoadState.Error) {
-                    item(
-                        span = { GridItemSpan(currentLineSpan = columnsSize) },
-                        content = {
-                            ButtonComposable(
-                                modifier = Modifier.wrapContentWidth(align = Alignment.CenterHorizontally),
-                                text = "Try again",
-                                onClick = {
-                                    lazyPagingItems.retry()
-                                }
-                            )
-                        }
-                    )
+
+                when (val state = lazyPagingItems.loadState.append) {
+                    is LoadState.Loading -> {
+                        item(
+                            span = { GridItemSpan(currentLineSpan = columnsSize) },
+                            content = {
+                                CircularProgressIndicator(
+                                    modifier = Modifier
+                                        .padding(all = 8.dp)
+                                        .wrapContentWidth(align = Alignment.CenterHorizontally)
+                                )
+                            }
+                        )
+                    }
+                    is LoadState.NotLoading -> if (state.endOfPaginationReached) {
+                        item(
+                            span = { GridItemSpan(currentLineSpan = columnsSize) },
+                            content = {
+                                Text(
+                                    modifier = Modifier
+                                        .padding(all = 8.dp)
+                                        .wrapContentWidth(align = Alignment.CenterHorizontally),
+                                    text = "That's enough scrolling for now :)",
+                                    fontWeight = FontWeight.Bold,
+                                    color = MaterialTheme.colors.primary
+                                )
+                            }
+                        )
+                    }
+                    is LoadState.Error -> {
+                        item(
+                            span = { GridItemSpan(currentLineSpan = columnsSize) },
+                            content = {
+                                ButtonComposable(
+                                    modifier = Modifier.wrapContentWidth(align = Alignment.CenterHorizontally),
+                                    text = "Try again",
+                                    onClick = {
+                                        lazyPagingItems.retry()
+                                    }
+                                )
+                            }
+                        )
+                    }
                 }
             }
         )
