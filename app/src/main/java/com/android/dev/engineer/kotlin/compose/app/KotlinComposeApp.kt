f221ed023b2209ebff551b1b93814e6e1310cc17
android dev engineer <>
2023-05-22 18:39:27 +0100

    add timber config

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/app/KotlinComposeApp.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/app/KotlinComposeApp.kt
@@ -1,7 +1,21 @@
 package com.android.dev.engineer.kotlin.compose.app
 
 import android.app.Application
+import com.android.dev.engineer.kotlin.compose.BuildConfig
+import com.android.dev.engineer.kotlin.compose.data.timber.TimberDebugTree
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class KotlinComposeApp : Application()
+class KotlinComposeApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setUpTimber()
+    }
+
+    private fun setUpTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(TimberDebugTree())
+        }
+    }
+}
