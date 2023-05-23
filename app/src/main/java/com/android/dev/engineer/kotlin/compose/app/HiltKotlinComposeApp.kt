7deae57869443b2418ea828f05ede5dfbdf6b2ea
android dev engineer <>
2023-05-23 22:02:28 +0100

    make application class testable

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/app/HiltKotlinComposeApp.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/app/HiltKotlinComposeApp.kt
@@ -3,11 +3,15 @@ package com.android.dev.engineer.kotlin.compose.app
 import android.app.Application
 import com.android.dev.engineer.kotlin.compose.BuildConfig
 import com.android.dev.engineer.kotlin.compose.data.timber.TimberDebugTree
+import com.android.dev.engineer.kotlin.compose.util.ExcludeFromJacocoGeneratedReport
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class KotlinComposeApp : Application() {
+@ExcludeFromJacocoGeneratedReport
+class HiltKotlinComposeApp : KotlinComposeApp()
+
+open class KotlinComposeApp : Application() {
     override fun onCreate() {
         super.onCreate()
         setUpTimber()
