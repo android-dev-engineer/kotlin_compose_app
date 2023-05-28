2d491c46c2a31bdb18b47380eb6a4785d87861e4
android dev engineer <>
2023-05-28 14:55:54 +0100

    add comment to explain Hilt gradle plugin issue with jacoco

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/app/KotlinComposeApp.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/app/KotlinComposeApp.kt
@@ -6,6 +6,7 @@ import com.android.dev.engineer.kotlin.compose.data.timber.TimberDebugTree
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+// Hilt gradle plugin causes Jacoco coverage issue: https://github.com/google/dagger/issues/1982
 @HiltAndroidApp(Application::class)
 class KotlinComposeApp : Hilt_KotlinComposeApp() {
     override fun onCreate() {
